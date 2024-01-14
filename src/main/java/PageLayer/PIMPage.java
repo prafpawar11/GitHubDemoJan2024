e9b660cb584cf9dc2b114354b3550148fa5aeb16
prafpawar11 <prafpawar11@gmail.com>
2024-01-14 08:57:14 +0530

    PIM Page url TC

---
+++ src/main/java/PageLayer/PIMPage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+public class PIMPage {
+
+	
+	public String getUrl()
+	{
+		return "https://www.orangehrm.com/pim";
+	}
+	
+}
---
+++ src/test/java/TestLayer/PIMPageTest.java
@@ -0,0 +1,22 @@
+package TestLayer;
+
+import PageLayer.PIMPage;
+
+public class PIMPageTest {
+
+	public static void main(String[] args) {
+		
+		PIMPage pimpage =new PIMPage();
+		
+		if(pimpage.getUrl().contains("pim"))
+		{
+			System.out.println("URL TC Pass");
+		}
+		else
+		{
+			System.out.println("URL TC fail");
+		}
+		
+	}
+	
+}
